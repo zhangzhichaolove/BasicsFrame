9d191208fbba14c53a85da9962f3ce3e20f8b055
zhangzhichao <zhangzhichaolove@vip.qq.com>
2018-03-08 16:52:30 +0800

    add UtilCode

--- app/src/main/java/com/peak/chao/basicsframe/App.java
+++ app/src/main/java/com/peak/chao/basicsframe/App.java
@@ -2,6 +2,7 @@ package com.peak.chao.basicsframe;
 
 import android.app.Application;
 
+import com.blankj.utilcode.util.Utils;
 import com.peak.chao.basicsframe.main.BasicsFrame;
 
 /**
@@ -12,6 +13,7 @@ public class App extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
+        Utils.init(this);
         BasicsFrame.getInstance().init(this);
     }
 }
